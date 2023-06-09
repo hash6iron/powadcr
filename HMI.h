650735355a4455c49baf19e50a696c85bdd1fbb5
hash6iron <hash6iron@gmail.com>
2023-06-09 18:46:54 +0200

    New updated
    Add new animation on HMI

--- HMI.h
+++ HMI.h
@@ -1,203 +1,193 @@
-void writeString(String stringData) 
-{ 
+void writeString(String stringData) {
 
-    // Controlamos el buffer overflow.
-    if ((stringData.length() + Serial.available()) < 64)
-    {
-        for (int i = 0; i < stringData.length(); i++)
-        {
-          // Enviamos los datos
-          Serial.write(stringData[i]);   
-        }
+  // Controlamos el buffer overflow.
+  if ((stringData.length() + Serial.available()) < 64) {
+    for (int i = 0; i < stringData.length(); i++) {
+      // Enviamos los datos
+      Serial.write(stringData[i]);
+    }
 
-        // Indicamos a la pantalla que ya hemos enviado todos 
-        // los datos, con este triple byte 0xFF
-        Serial.write(0xff); 
-        Serial.write(0xff); 
-        Serial.write(0xff);
-    }
-    else
-    {
-        Serial.flush();
-        Serial.println("");
-        Serial.println("OVERFLOW FLUSHED!!!");
-        Serial.println("");
-    }
+    // Indicamos a la pantalla que ya hemos enviado todos
+    // los datos, con este triple byte 0xFF
+    Serial.write(0xff);
+    Serial.write(0xff);
+    Serial.write(0xff);
+  } else {
+    Serial.flush();
+    Serial.println("");
+    Serial.println("OVERFLOW FLUSHED!!!");
+    Serial.println("");
+  }
 }
 
-void updateInformationMainPage()
-{
+void updateInformationMainPage() {
 
-    if (TOTAL_BLOCKS != 0)
-    {
+  if (TOTAL_BLOCKS != 0) {
 
-        // Cargamos la información
-        LAST_NAME = globalTAP.descriptor[BLOCK_SELECTED].name;
-        LAST_SIZE = globalTAP.descriptor[BLOCK_SELECTED].size;
-        LAST_TYPE = globalTAP.descriptor[BLOCK_SELECTED].typeName;
+    // Cargamos la información
+    LAST_NAME = globalTAP.descriptor[BLOCK_SELECTED].name;
+    LAST_SIZE = globalTAP.descriptor[BLOCK_SELECTED].size;
+    LAST_TYPE = globalTAP.descriptor[BLOCK_SELECTED].typeName;
 
-        // Enviamos información al HMI
-        writeString("");
-        writeString("name.txt=\"" + PROGRAM_NAME + "\"");
-
-        writeString("");
-        writeString("size.txt=\"" + String(LAST_SIZE-2) + " bytes\"");
-
-        writeString("");
-        writeString("totalBlocks.val=" + String(TOTAL_BLOCKS));
-
-        writeString("");
-        writeString("currentBlock.val=" + String(BLOCK_SELECTED+1));
-
-        writeString("");
-        writeString("type.txt=\""+ String(LAST_TYPE) + ":" + String(LAST_NAME) + "\"");
-    }
+    // Enviamos información al HMI
+    writeString("");
+    writeString("name.txt=\"" + PROGRAM_NAME + "\"");
+    writeString("");
+    writeString("screen2.name.txt=\"" + PROGRAM_NAME + "\"");
 
     writeString("");
-    writeString("g0.txt=\"" + LAST_MESSAGE + "\"");
+    writeString("size.txt=\"" + String(LAST_SIZE - 2) + " bytes\"");
+    writeString("");
+    writeString("screen2.size.txt=\"" + String(LAST_SIZE - 2) + " bytes\"");
+
+    writeString("");
+    writeString("type.txt=\"" + String(LAST_TYPE) + ":" + String(LAST_NAME) + "\"");
+    writeString("");
+    writeString("screen2.type.txt=\"" + String(LAST_TYPE) + ":" + String(LAST_NAME) + "\"");
+
+    writeString("");
+    writeString("totalBlocks.val=" + String(TOTAL_BLOCKS));
+    writeString("");
+    writeString("screen2.totalBlocks.val=" + String(TOTAL_BLOCKS));
+
+    writeString("");
+    writeString("currentBlock.val=" + String(BLOCK_SELECTED + 1));
+    writeString("");
+    writeString("screen2.currentBlock.val=" + String(BLOCK_SELECTED + 1));
+  }
+
+  writeString("");
+  writeString("g0.txt=\"" + LAST_MESSAGE + "\"");
 }
 
-void verifyCommand(String strCmd)
-{
-    
-    if(strCmd.indexOf("LCDON") != -1)
-    {
-        LCD_ON = true;
-    }
-    
-    if (strCmd.indexOf("FFWD") != -1)
-    {
-        if (LOADING_STATE == 2 || LOADING_STATE == 0)
-        {
-            BLOCK_SELECTED++;
+void verifyCommand(String strCmd) {
+  // if (strCmd.indexOf("BKX=")) {
+  //   //Cogemos el valor
+  //   byte buff[7];
+  //   strCmd.getBytes(buff, 7);
+  //   //BLOCK_SELECTED = buff[4]) * 1000 + ((int)buff[5]) * 100 + ((int)buff[6]) * 10 + ((int)buff[7]);
+  //   String num = String(buff[4]+buff[5]+buff[6]+buff[7]);
+  //   BLOCK_SELECTED = num.toInt();
 
-            if (BLOCK_SELECTED > TOTAL_BLOCKS-1)
-            {
-                BLOCK_SELECTED = 0;
-            }
+  //   updateInformationMainPage();
+  // }
 
-            updateInformationMainPage();      
-        }
+  if (strCmd.indexOf("LCDON") != -1) {
+    LCD_ON = true;
+  }
 
-    }
+  if (strCmd.indexOf("FFWD") != -1) {
+    if (LOADING_STATE == 2 || LOADING_STATE == 0) {
+      BLOCK_SELECTED++;
 
-    if (strCmd.indexOf("RWD") != -1)
-    {
-        if (LOADING_STATE == 2 || LOADING_STATE == 0)
-        {
-
-            BLOCK_SELECTED--;
-
-            if (BLOCK_SELECTED < 0)
-            {
-                BLOCK_SELECTED = TOTAL_BLOCKS-1;
-            }
-
-            updateInformationMainPage();           
-        }
-    }
-
-    if (strCmd.indexOf("PLAY") != -1)
-    {
-        PLAY = true;
-        PAUSE = false;
-        STOP = false;
-
-        Serial.println("");
-        Serial.print("PLAY pressed");
-
-        LAST_MESSAGE = "Loading in progress. Please wait.";
-        updateInformationMainPage();           
-    }
-
-    if (strCmd.indexOf("PAUSE") != -1)
-    {
-        PLAY = false;
-        PAUSE = true;
-        STOP = false;
-
-        Serial.println("");
-        Serial.print("PAUSE pressed");
-
-        LAST_MESSAGE = "Tape paused. Press play to continue load.";
-        updateInformationMainPage();           
-    }
-
-    if (strCmd.indexOf("STOP") != -1)
-    {
-        PLAY = false;
-        PAUSE = false;
-        STOP = true;
+      if (BLOCK_SELECTED > TOTAL_BLOCKS - 1) {
         BLOCK_SELECTED = 0;
-        BYTES_LOADED = 0;
+      }
 
-        Serial.println("");
-        Serial.print("STOP pressed");    
+      updateInformationMainPage();
+    }
+  }
 
-        LAST_MESSAGE = "Tape stop. Press play to start again.";
-        writeString("");
-        writeString("currentBlock.val=1");   
-        writeString("");
-        writeString("progressTotal.val=0");      
-        writeString("");
-        writeString("progression.val=0");                   
-        updateInformationMainPage();           
+  if (strCmd.indexOf("RWD") != -1) {
+    if (LOADING_STATE == 2 || LOADING_STATE == 0) {
+
+      BLOCK_SELECTED--;
+
+      if (BLOCK_SELECTED < 0) {
+        BLOCK_SELECTED = TOTAL_BLOCKS - 1;
+      }
+
+      updateInformationMainPage();
+    }
+  }
+
+  if (strCmd.indexOf("PLAY") != -1) {
+    PLAY = true;
+    PAUSE = false;
+    STOP = false;
+
+    Serial.println("");
+    Serial.print("PLAY pressed");
+
+    LAST_MESSAGE = "Loading in progress. Please wait.";
+    updateInformationMainPage();
+  }
+
+  if (strCmd.indexOf("PAUSE") != -1) {
+    PLAY = false;
+    PAUSE = true;
+    STOP = false;
+
+    Serial.println("");
+    Serial.print("PAUSE pressed");
+
+    LAST_MESSAGE = "Tape paused. Press play to continue load.";
+    updateInformationMainPage();
+  }
+
+  if (strCmd.indexOf("STOP") != -1) {
+    PLAY = false;
+    PAUSE = false;
+    STOP = true;
+    BLOCK_SELECTED = 0;
+    BYTES_LOADED = 0;
+
+    Serial.println("");
+    Serial.print("STOP pressed");
+
+    LAST_MESSAGE = "Tape stop. Press play to start again.";
+    writeString("");
+    writeString("currentBlock.val=1");
+    writeString("");
+    writeString("progressTotal.val=0");
+    writeString("");
+    writeString("progression.val=0");
+    updateInformationMainPage();
+  }
+
+  if (strCmd.indexOf("VOL") != -1) {
+    //Cogemos el valor
+    byte buff[7];
+    strCmd.getBytes(buff, 7);
+    int valVol = (int)buff[4];
+    MAIN_VOL = valVol;
+    ESP32kit.setVolume(MAIN_VOL);
+  }
+
+  if (strCmd.indexOf("TXT_SEARCH") != -1) {
+    //Cogemos el valor
+    byte buff[50];
+    strCmd.getBytes(buff, 50);
+    int n = 12;
+    char phrase[50];
+    char str = (char)buff[n];
+    while (str != '@') {
+      phrase[n - 12] += (char)buff[n];
+      n++;
+      str = (char)buff[n];
     }
 
-    if (strCmd.indexOf("VOL") != -1)
-    {
-        //Cogemos el valor
-        byte buff[7];
-        strCmd.getBytes(buff, 7);
-        int valVol = (int)buff[4];
-        MAIN_VOL = valVol;
-        ESP32kit.setVolume(MAIN_VOL);
-    }
-
-    if (strCmd.indexOf("TXT_SEARCH") != -1)
-    {
-        //Cogemos el valor
-        byte buff[50];
-        strCmd.getBytes(buff, 50);
-        int n = 12;
-        char phrase[50];
-        char str=(char)buff[n];
-        while (str !='@')
-        {
-            phrase[n-12] += (char)buff[n];
-            n++;
-            str = (char)buff[n];
-        }
-
-        Serial.println("TXT TO SEARCH: " + String(phrase));
-    }
-
+    Serial.println("TXT TO SEARCH: " + String(phrase));
+  }
 }
 
-void readUART()
-{
+void readUART() {
 
 
-  if (Serial.available() > 0) 
-  {
+  if (Serial.available() > 0) {
     // get the new byte:
     String strCmd = Serial.readString();
     verifyCommand(strCmd);
     //Serial.flush();
-  }  
+  }
 }
 
-void serialSendData(String data)
-{
+void serialSendData(String data) {
 
-    if (Serial.available() + data.length() < 64)
-    { 
-      // Lo envio
-      Serial.println(data);
-    }
-    else
-    {
-      // Lo descarto
-    }
+  if (Serial.available() + data.length() < 64) {
+    // Lo envio
+    Serial.println(data);
+  } else {
+    // Lo descarto
+  }
 }
-
