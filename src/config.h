3abeb3176f36ef636da22eae05fb9c472960320e
hash6iron <hash6iron@gmail.com>
2025-04-10 03:06:58 +0200

    modified:   src/config.h

--- src/config.h
+++ src/config.h
@@ -37,7 +37,7 @@
 
 // Para Rolling releases - rDDMMYY.HHMM
 // Para versión estable - vX.Y
-#define VERSION "v1.0r2-20250408-2"
+#define VERSION "v1.0r4"
 #define MACHINE_ZX
 
 // Define sampling rate a 44.1KHz. En otro caso será a 32KHz
