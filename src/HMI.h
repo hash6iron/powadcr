88dfd620d4613a8cc6be42ed5cb6fb5e7fbc6c6b
hash6iron <hash6iron@gmail.com>
2026-05-13 14:21:51 +0200

    modified:   src/HMI.h

--- src/HMI.h
+++ src/HMI.h
@@ -3204,7 +3204,7 @@ private:
           // Le cambiamos el signo para que cuando seleccionemos -1 sea bajar tono en vez de quitar una muestra que es todo lo contrario (aumentar frecuencia)
           // y lo mismo con el +1
           AZIMUT = valVol;
-          C64_TIMING_BIAS = AZIMUT * (BIAS_FACTOR_TAP_C64);
+          C64_TIMING_BIAS = (AZIMUT - 5) * (BIAS_FACTOR_TAP_C64);
           logln("C64_TIMING_BIAS: " + String(C64_TIMING_BIAS, 8) + " samples");
 
           //TONE_ADJUST = (-210)*(TONE_ADJUSTMENT_ZX_SPECTRUM + (valVol-TONE_ADJUSTMENT_ZX_SPECTRUM_LIMIT));         
